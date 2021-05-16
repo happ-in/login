23ece750b0dbd0d3ce97c4cef1038f24a223d6e3
hyebinppark <gpqls304@naver.com>
2021-05-16 23:02:41 +0900

    add: Login Vue

---
+++ src/views/MainLogin.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <form class="loginForm">
+      <h2>Login</h2>
+      <div class="inputDiv">
+        <input type="text" class="inputForm" placeholder="ID" v-model="id" />
+      </div>
+      <div class="inputDiv">
+        <input type="password" class="inputForm" placeholder="PW" v-model="password" />
+      </div>
+      <button type="button" class="btn" onclick="button()">LOG IN</button>
+      <div class="bottomText">
+        <router-link to="/join">sign up </router-link> |
+        <router-link to="/"> forgot password</router-link>
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      id: "",
+      password: "",
+    };
+  },
+};
+</script>
+<style>
+@import "./css/Main.css";
+</style>
