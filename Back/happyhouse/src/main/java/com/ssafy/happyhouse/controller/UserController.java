bef91941b6cfdc723ea6e4ed0b8e8a2ed605f3d3
hyebinppark <gpqls304@naver.com>
2021-05-17 21:55:37 +0900

    fix: cross origin

--- Back/happyhouse/src/main/java/com/ssafy/happyhouse/controller/UserController.java
+++ Back/happyhouse/src/main/java/com/ssafy/happyhouse/controller/UserController.java
@@ -7,13 +7,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin("*")
 public class UserController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
