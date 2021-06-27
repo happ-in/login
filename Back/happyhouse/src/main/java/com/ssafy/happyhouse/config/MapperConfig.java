45b16373c9237b0aeaff3835166ec06d5d9536fb
hyebinppark <gpqls304@naver.com>
2021-06-27 23:07:03 +0900

    init

--- Back/happyhouse/src/main/java/com/ssafy/happyhouse/config/MapperConfig.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.happyhouse.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan(basePackages = "com.ssafy.happyhouse.model.repository")
-public class MapperConfig {
-}
--- Back/happyhouse/src/main/java/com/ssafy/happyhouse/model/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.happyhouse.model.repository;
-
-import com.ssafy.happyhouse.model.domain.User;
-
-public interface UserRepository {
-    User login(User user);
-
-    User findByUserName(String username);
-}
